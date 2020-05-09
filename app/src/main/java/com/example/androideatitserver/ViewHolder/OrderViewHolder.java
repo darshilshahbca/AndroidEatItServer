6652e80bf1d1012802483c6630e9f5bbcfd5f65b
darshilshahbca <darshil.saphcm@gmail.com>
2020-05-09 11:49:58 +0530

    -Reverted LongClick Logic commented

--- app/src/main/java/com/example/androideatitserver/ViewHolder/OrderViewHolder.java
+++ app/src/main/java/com/example/androideatitserver/ViewHolder/OrderViewHolder.java
@@ -12,7 +12,7 @@ import com.example.androideatitserver.Interface.ItemClickListener;
 import com.example.androideatitserver.R;
 
 public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
-//                            View.OnLongClickListener,
+                            View.OnLongClickListener,
                             View.OnCreateContextMenuListener{
 
     public TextView txtOrderId, txtOrderStatus, txtOrderPhone, txtOrderAddress;
@@ -28,7 +28,7 @@ public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnC
         txtOrderPhone = (TextView)itemView.findViewById(R.id.order_phone);
 
         itemView.setOnCreateContextMenuListener(this);
-//        itemView.setOnLongClickListener(this); //Commented as Custom Code Added for Single Click
+        itemView.setOnLongClickListener(this);
         itemView.setOnClickListener(this);
 
     }
@@ -49,11 +49,11 @@ public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnC
         menu.add(0, 0, getAdapterPosition(), Common.DELETE);
     }
 
-//    @Override
-//    public boolean onLongClick(View view) {
-//        itemClickListener.onClick(view, getAdapterPosition(), true);
-//
-//        return true;
-//    }
+    @Override
+    public boolean onLongClick(View view) {
+        itemClickListener.onClick(view, getAdapterPosition(), true);
+
+        return true;
+    }
 }
 
